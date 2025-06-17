88e8c82115af4634d1209a896dd909a73bb4ba35
afole <koladeafo@gmail.com>
2025-06-17 20:35:02 +0100

    functionality works

--- playlist/serializers.py
+++ playlist/serializers.py
@@ -7,6 +7,8 @@ class PlaylistSerializer(serializers.ModelSerializer):
          many=True,
          required=False
     )
+    song_id = serializers.IntegerField(write_only=True, required=False)
+    remove_song_id = serializers.IntegerField(write_only=True, required=False)
 
     class Meta:
         model = playlist
@@ -14,8 +16,29 @@ class PlaylistSerializer(serializers.ModelSerializer):
         read_only_fields = ['owner']
         depth = 1
 
-    # def create(self, validated_data):
-    #     songs = validated_data.pop('songs', [])
-    #     playlist_instance = playlist.objects.create(**validated_data)
-    #     playlist_instance.songs.set(songs)
-    #     return playlist_instance
+    def create(self, validated_data):
+        songs = validated_data.pop('songs', [])
+        playlist_instance = playlist.objects.create(**validated_data)
+        playlist_instance.songs.set(songs)
+        return playlist_instance
+
+
+    def update(self, instance, validated_data):
+        song_id = validated_data.pop('song_id', None)
+        if song_id:
+            try:
+                song = music.objects.get(id=song_id)
+                instance.songs.add(song)
+            except music.DoesNotExist:
+                raise serializers.ValidationError({"song_id": "Invalid song ID"})
+            
+        remove_song_id = validated_data.pop('remove_song_id', None)
+        if remove_song_id:
+            try:
+                song = music.objects.get(id=remove_song_id)
+                instance.songs.remove(song)
+            except music.DoesNotExist:
+                raise serializers.ValidationError({"remove_song_id": "Invalid song ID"})
+        
+        instance.save()
+        return instance
